4e874a9c03199c95ad8dd2141843ce93c899b91c
Juliana Dias <juliana.dias.mayrink@gmail.com>
2026-09-11 20:28:29 +0200

    test: add service and endpoint tests for GET /user/{id} #6

---
+++ src/test/java/es/upm/miw/devops/integrationtests/UserServiceIT.java
@@ -0,0 +1,44 @@
+package es.upm.miw.devops.integrationtests;
+
+import es.upm.miw.devops.domain.model.User;
+import es.upm.miw.devops.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class UserServiceIT {
+
+    private static final UUID EXISTING_USER_ID = UUID.fromString("00000000-0000-0000-0000-000000000001");
+    private static final UUID UNKNOWN_USER_ID = UUID.fromString("00000000-0000-0000-0000-000000000999");
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    void testReadExistingUser() {
+        User user = this.userService.read(EXISTING_USER_ID);
+        assertThat(user).isNotNull();
+        assertThat(user.getId()).isEqualTo(EXISTING_USER_ID);
+        assertThat(user.getFirstName()).isEqualTo("Ana");
+    }
+
+    @Test
+    void testReadUnknownUserThrowsNotFound() {
+        assertThatThrownBy(() -> this.userService.read(UNKNOWN_USER_ID))
+                .isInstanceOf(ResponseStatusException.class)
+                .satisfies(exception -> {
+                    ResponseStatusException responseStatusException = (ResponseStatusException) exception;
+                    assertThat(responseStatusException.getStatusCode()).isEqualTo(NOT_FOUND);
+                });
+    }
+}
